1cc95e5df95eb0043f88766cd8f184aa7677f331
Stéphane Sallaberry <ssallaberry@appstud.com>
2017-06-01 17:20:34 +0200

    style on NFC confirmation

--- app/src/main/java/com/appstud/parking/NFCActivity.java
+++ app/src/main/java/com/appstud/parking/NFCActivity.java
@@ -2,7 +2,6 @@ package com.appstud.parking;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class NFCActivity extends AppCompatActivity {
 
@@ -10,8 +9,6 @@ public class NFCActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
