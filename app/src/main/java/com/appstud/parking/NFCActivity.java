ebd0f252d945512d4c99cca1d98df988040e04bd
Stéphane Sallaberry <ssallaberry@appstud.com>
2017-06-01 17:26:13 +0200

    close NFC modal

--- app/src/main/java/com/appstud/parking/NFCActivity.java
+++ app/src/main/java/com/appstud/parking/NFCActivity.java
@@ -3,12 +3,20 @@ package com.appstud.parking;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class NFCActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.parking_close)
+    void closeConfirmation() {
+        this.finish();
+    }
 }
