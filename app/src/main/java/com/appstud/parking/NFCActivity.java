b1cc7604ffed229929d26f24e4887c96cabaa3b7
Stéphane Sallaberry <ssallaberry@appstud.com>
2017-06-09 16:52:54 +0200

    style on parking entry and exit

--- app/src/main/java/com/appstud/parking/NFCActivity.java
+++ app/src/main/java/com/appstud/parking/NFCActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,8 +14,11 @@ import static com.appstud.parking.MyHostApduService.IS_PARKING_EXIT;
 
 public class NFCActivity extends AppCompatActivity {
 
-    @BindView(R.id.parking_ticket_details)
-    LinearLayout parkingTicketDetails;
+    @BindView(R.id.exit_description)
+    LinearLayout exitDescription;
+
+    @BindView(R.id.entry_description)
+    LinearLayout entryDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,11 @@ public class NFCActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         if (isParkingExit()) {
-            parkingTicketDetails.setVisibility(View.VISIBLE);
+            exitDescription.setVisibility(View.VISIBLE);
+            entryDescription.setVisibility(View.GONE);
         } else {
-            parkingTicketDetails.setVisibility(View.GONE);
+            exitDescription.setVisibility(View.GONE);
+            entryDescription.setVisibility(View.VISIBLE);
         }
     }
 
@@ -36,5 +42,11 @@ public class NFCActivity extends AppCompatActivity {
     @OnClick(R.id.parking_close)
     void closeConfirmation() {
         this.finish();
+    }
+
+
+    @OnClick(R.id.parking_receipt_btn)
+    void getReceipt() {
+        Toast.makeText(this, R.string.get_parking_receipt_toast, Toast.LENGTH_SHORT).show();
     }
 }
